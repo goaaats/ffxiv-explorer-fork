fe0c235d4e758cda96d792e0b33673fddbb6b75f
Filip Maj <filipmaj@gmail.com>
2014-06-17 11:53:49 -0400

    Removed Java 7 stuff so it works on Java 6.

--- src/ca/fraggergames/ffxivextract/Main.java
+++ src/ca/fraggergames/ffxivextract/Main.java
@@ -16,8 +16,7 @@ public class Main {
 	public static void main(String[] args) {
 		try {
 			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-		} catch (ClassNotFoundException | InstantiationException
-				| IllegalAccessException | UnsupportedLookAndFeelException e) {
+		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
