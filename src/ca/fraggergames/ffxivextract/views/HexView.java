d956fa58f71f83063d8879b6b1a385c54729fb8a
Filip Maj <filipmaj@gmail.com>
2014-06-17 11:55:46 -0400

    New hex editor view. This will be used for unknown bin files.

---
+++ src/ca/fraggergames/ffxivextract/views/HexView.java
@@ -0,0 +1,73 @@
+package ca.fraggergames.ffxivextract.views;
+
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+
+public class HexView extends JPanel{
+
+	JTextArea txtHexData = new JTextArea();
+	
+	public HexView(byte[] byteArray, int columnCount)
+	{
+		createTextAreaBody(byteArray, columnCount);
+		add(txtHexData);
+	}
+
+	private void createTextAreaBody(byte[] byteArray, int columnCount) {
+		String data = "";
+		int valBeginofLastRow = 0;
+		int total;
+		if (byteArray.length < columnCount) 
+			total = columnCount; 
+		else
+			total = byteArray.length + (byteArray.length % columnCount);  
+		
+		for (int i = 0; i < total; i++)
+		{										
+			//New Row
+			if (i % columnCount == 0)
+			{				
+				//Ignore if just started
+				if (i != 0)
+				{
+					//Do ASCII and new row
+					for (int j = valBeginofLastRow; j < i; j++)
+					{
+						if (j > byteArray.length -1)
+							data += 'x';
+						else if (byteArray[j] <= 32 || byteArray[j] >= 126)
+							data += '.';
+						else
+							data += (char) byteArray[j];
+					}
+					data += "\n";
+				}				
+				valBeginofLastRow = i;				
+				data += String.format("0x%08X: ", i);
+			}
+			
+			if (i > byteArray.length-1)
+				data += "XX ";
+			else
+				data += String.format("%02X ", byteArray[i]);
+			
+		}
+		
+		//Left over stuff incase the mod isn't exact
+				
+			for (int i = valBeginofLastRow; i < byteArray.length + (byteArray.length % columnCount); i++)
+			{
+				if (i > byteArray.length -1)
+					data += ' ';
+				else if (byteArray[i] <= 32 || byteArray[i] >= 126)
+					data += '.';
+				else
+					data += (char) byteArray[i];
+			}
+					
+		
+		
+		txtHexData.setText(data);
+	}
+			
+}
