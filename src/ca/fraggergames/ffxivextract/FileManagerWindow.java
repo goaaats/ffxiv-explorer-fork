7f028a58e211c40f625e9682fc9b44ebe5269b02
Filip Maj <filipmaj@gmail.com>
2014-06-18 00:56:30 -0400

    Extraction and Mass Extraction done.

--- src/ca/fraggergames/ffxivextract/FileManagerWindow.java
+++ src/ca/fraggergames/ffxivextract/FileManagerWindow.java
@@ -4,7 +4,9 @@ import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
@@ -17,7 +19,9 @@ import javax.swing.event.TreeSelectionEvent;
 import javax.swing.event.TreeSelectionListener;
 import javax.swing.filechooser.FileFilter;
 
+import ca.fraggergames.ffxivextract.helpers.LERandomAccessFile;
 import ca.fraggergames.ffxivextract.models.SqPack_DatFile;
+import ca.fraggergames.ffxivextract.models.SqPack_File;
 import ca.fraggergames.ffxivextract.models.SqPack_IndexFile;
 import ca.fraggergames.ffxivextract.views.HexView;
 
@@ -55,8 +59,8 @@ public class FileManagerWindow extends JFrame implements TreeSelectionListener {
 		this.setTitle(title);
 		this.getContentPane().add(splitPane);		
 		
-		lastOpenedFile = new File("G:\\FINAL FANTASY XIV - A Realm Reborn\\game\\sqpack\\ffxiv\\000000.win32.index");
-		openFile(lastOpenedFile);
+		//lastOpenedFile = new File("G:\\FINAL FANTASY XIV - A Realm Reborn\\game\\sqpack\\ffxiv\\000000.win32.index");
+		//openFile(lastOpenedFile);
 	}	
 
 	protected void openFile(File selectedFile) {
@@ -121,28 +125,37 @@ public class FileManagerWindow extends JFrame implements TreeSelectionListener {
 			{
 				closeFile();				
 			}
+			else if (event.getActionCommand().equals("extract"))
+			{
+				extractSelected();
+			}
 			else if (event.getActionCommand().equals("quit"))
 			{
 				System.exit(0);
 			}
-		}
+		}		
 	};
 
 	private void setupMenu(){		
 		
 		//File Menu
-		JMenu file = new JMenu("File");		
+		JMenu file = new JMenu("File");				
 		JMenuItem file_Open = new JMenuItem("Open");
 		file_Open.setActionCommand("open");
 		JMenuItem file_Close = new JMenuItem("Close");
 		file_Close.setActionCommand("close");
+		JMenuItem file_Extract = new JMenuItem("Extract");
+		file_Extract.setActionCommand("extract");
 		JMenuItem file_Quit = new JMenuItem("Quit");
 		file_Quit.setActionCommand("quit");
 		file_Open.addActionListener(menuHandler);
-		file_Close.addActionListener(menuHandler);	
+		file_Close.addActionListener(menuHandler);
+		file_Extract.addActionListener(menuHandler);
 		file_Quit.addActionListener(menuHandler);
+		
 		file.add(file_Open);
 		file.add(file_Close);
+		file.add(file_Extract);		
 		file.add(file_Quit);	
 		
 		//Super Menus
@@ -166,5 +179,61 @@ public class FileManagerWindow extends JFrame implements TreeSelectionListener {
 			e1.printStackTrace();
 		}
 	}	
+	
+	private void extractSelected() {
+		JFileChooser fileChooser = new JFileChooser(lastOpenedFile);
+		
+		ArrayList<SqPack_File> files = fileTree.getSelectedFiles();
+		
+		if (files.size() > 1)
+			fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+		
+		fileChooser.setFileFilter(new FileFilter() {
+			
+			@Override
+			public String getDescription() {
+				return null;
+			}
+			
+			@Override
+			public boolean accept(File f) {
+				return f.getName().endsWith(".index") || f.isDirectory();
+			}				
+		});
+		int retunval = fileChooser.showSaveDialog(FileManagerWindow.this);
+		if (retunval == JFileChooser.APPROVE_OPTION)
+		{
+			lastOpenedFile = fileChooser.getSelectedFile();
+			
+			if (files.size() == 1)
+			{
+				try {
+					byte[] data = currentDatFile.extractFile(files.get(0).getOffset());
+					LERandomAccessFile out = new LERandomAccessFile(lastOpenedFile, "rw");
+					out.write(data, 0, data.length);
+					out.close();
+				} catch (FileNotFoundException e) {
+					e.printStackTrace();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			else
+			{
+				for (int i = 0; i < files.size(); i++){
+					try {
+						byte[] data = currentDatFile.extractFile(files.get(i).getOffset());
+						LERandomAccessFile out = new LERandomAccessFile(lastOpenedFile.getAbsolutePath() + "\\" + String.format("%X", files.get(i).getId() & 0xFFFFFFFF) + ".dat", "rw");
+						out.write(data, 0, data.length);
+						out.close();
+					} catch (FileNotFoundException e) {
+						e.printStackTrace();
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		}
+	}
 
 }
