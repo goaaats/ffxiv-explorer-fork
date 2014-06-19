f66d921937d032c6a048c04d95504a26d988e532
Filip Maj <filipmaj@gmail.com>
2014-06-18 23:40:08 -0400

    Added loader code for file. Added more info in research.

--- src/ca/fraggergames/ffxivextract/models/EXDF_File.java
+++ src/ca/fraggergames/ffxivextract/models/EXDF_File.java
@@ -1,44 +1,54 @@
 package ca.fraggergames.ffxivextract.models;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 
 public class EXDF_File {
 
 	public EXDF_Offset[] offsets;
-	
-	public EXDF_File(byte[] data) throws IOException
-	{
+
+	public EXDF_File(byte[] data) throws IOException {
+		loadEXDF(data);
+	}
+
+	public EXDF_File(String path) throws IOException, FileNotFoundException {
+		File file = new File(path);
+		FileInputStream fis = new FileInputStream(file);
+		byte[] data = new byte[(int) file.length()];
+		fis.read(data);
+		fis.close();
+		
+		loadEXDF(data);
+	}
+
+	private void loadEXDF(byte[] data) throws IOException{
 		ByteBuffer buffer = ByteBuffer.allocateDirect(data.length);
 		buffer.put(data);
 		buffer.rewind();
-		
-		//EXDF Header
-		int signature = buffer.getInt(); //Should be EXDF or 0x45584446
-		if (signature != 0)
+
+		// EXDF Header
+		int signature = buffer.getInt(); 
+		if (signature != 0x45584446) // Should be EXDF or 0x45584446
 			throw new IOException("Not a EXDF");
-		buffer.get(); //Sig is 5 bytes so skip 1
+		buffer.get(); // Sig is 5 bytes so skip 1
 		int version = buffer.getInt();
-		int offsetSize = buffer.getInt(); 
-		int dataSize = buffer.getInt();  
-		//Skip to 0x1F
-		
-		//Offsets
-		offsets = new EXDF_Offset[offsetSize/0x08];
-		for (int i = 0; i < offsets.length; i++)
-		{
+		int offsetSize = buffer.getInt();
+		int dataSize = buffer.getInt();
+		// Skip to 0x1F
+
+		// Offsets
+		offsets = new EXDF_Offset[offsetSize / 0x08];
+		for (int i = 0; i < offsets.length; i++) {
 			int x = buffer.getInt();
-			int offset = buffer.getInt();			
+			int offset = buffer.getInt();
 			offsets[i] = new EXDF_Offset(offset, x);
 		}
-		
-		//Data
+
+		// Data
 		
 	}	
-	
-	public EXDF_File(String path)
-	{
-		
-	}
 
 }
