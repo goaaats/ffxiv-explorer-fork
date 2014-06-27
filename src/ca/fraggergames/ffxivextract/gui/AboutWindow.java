28e1c5c800c0f59d93482e941642863ed96fd095
Filip Maj <filipmaj@gmail.com>
2014-06-26 23:08:44 -0400

    Finished the new About Window.

--- src/ca/fraggergames/ffxivextract/gui/AboutWindow.java
+++ src/ca/fraggergames/ffxivextract/gui/AboutWindow.java
@@ -1,7 +1,12 @@
 package ca.fraggergames.ffxivextract.gui;
 
 import java.awt.BorderLayout;
-import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.RenderingHints;
 import java.net.URL;
 
 import javax.swing.BoxLayout;
@@ -9,6 +14,7 @@ import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
 
 import ca.fraggergames.ffxivextract.Constants;
 
@@ -16,36 +22,72 @@ public class AboutWindow extends JFrame {
 
 	JPanel aboutWindow = new JPanel();
 	JPanel container = new JPanel();
-	JLabel appname = new JLabel(Constants.APPNAME);
-	JLabel author = new JLabel("By Magis Luagis on Excalibur");
-	JLabel version = new JLabel("Version: " + Constants.VERSION);
-	JLabel gitcommit = new JLabel("Git Commit: " + Constants.COMMIT.substring(0, 5));
-	JLabel lol = new JLabel("(Sorry, this was last minute)");
+	FancyJLabel appname = new FancyJLabel(Constants.APPNAME);	
+	JLabel author = new FancyJLabel("By Magis");
+	JLabel version = new FancyJLabel("Version: " + Constants.VERSION);
+	JLabel gitcommit = new FancyJLabel("Git Commit: " + Constants.COMMIT.substring(0, 5));
 	
 	JLabel magisImage = new JLabel();
 	
 	public AboutWindow() {
 		this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		this.setSize(480, 300);
+		this.setSize(480, 700);
 		this.setTitle("About " + Constants.APPNAME);
 		
 		container.setLayout(new BoxLayout(container, BoxLayout.Y_AXIS));
+	
+		Font titleFont = new Font("Helvetica", Font.BOLD, 20);
+		Font standardFont = new Font("Helvetica", Font.PLAIN, 14);
 		
-		ClassLoader cldr = this.getClass().getClassLoader();
-		URL imageURL = getClass().getResource("/res/magis.jpeg");
-		ImageIcon image = new ImageIcon(imageURL);
-		magisImage.setIcon(image);
-				
+		appname.setFont(titleFont);
+		author.setFont(standardFont);
+		version.setFont(standardFont);
+		gitcommit.setFont(standardFont);
+		
+		ImageIcon image = new ImageIcon(getClass().getResource("/res/me.png"));
+		magisImage.setIcon(image);				
+			
 		container.add(appname);
 		container.add(author);
 		container.add(version);
 		container.add(gitcommit);		
-		container.add(lol);
 		
-		aboutWindow.setLayout(new BorderLayout());
-		aboutWindow.add(container, BorderLayout.LINE_START);
+		container.setBorder(new EmptyBorder(10, 10, 10, 10));
+		
+		JPanel centerPanel = new JPanel();
+		GridBagConstraints gbc = new GridBagConstraints();
+		gbc.gridy = 1;
+		centerPanel.setLayout(new GridBagLayout());		
+		centerPanel.add(container, gbc);
+		
+		aboutWindow.setLayout(new BorderLayout());		
+		aboutWindow.add(centerPanel, BorderLayout.LINE_START);
 		aboutWindow.add(magisImage, BorderLayout.LINE_END);
 		getContentPane().add(aboutWindow);
+		
+		ImageIcon icon = new ImageIcon(getClass().getResource("/res/frameicon.png"));
+		
+		this.setIconImage(icon.getImage());
+		this.pack();
+		this.setSize(getWidth(), getHeight()-10);
+		this.setResizable(false);
+	}
+
+	private class FancyJLabel extends JLabel{
+	
+		public FancyJLabel(String string)
+		{
+			super(string);
+		}
+
+            @Override
+            public void paintComponent(Graphics g) {
+                Graphics2D graphics2d = (Graphics2D) g;
+                graphics2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+                        RenderingHints.VALUE_ANTIALIAS_ON);
+                super.paintComponent(g);
+            }
+        
 	}
 	
 }
