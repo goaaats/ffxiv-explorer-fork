21f571842d39ba7e69467d32a7aedbe36c64845d
Filip Maj <filipmaj@gmail.com>
2014-06-28 02:16:52 -0400

    New version's commit

--- src/ca/fraggergames/ffxivextract/Constants.java
+++ src/ca/fraggergames/ffxivextract/Constants.java
@@ -4,7 +4,7 @@ public class Constants {
 	
 	public static final String APPNAME = "FFXIV 2.0 Data Explorer";
 	public static final String VERSION = "v0.11";
-	public static final String COMMIT = "28e1c5c800c0f59d93482e941642863ed96fd095";
+	public static final String COMMIT = "07735cbbae203569a0cefe006bd6104c71653377";
 	public static boolean DEBUG = false;
 	
 }
