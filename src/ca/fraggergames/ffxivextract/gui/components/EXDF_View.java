04601592cdda15044e6d7568270dc6a5657bcb1a
Filip Maj <filipmaj@gmail.com>
2014-07-01 16:00:35 -0400

    Added auto sizing of table for EXDF view.

--- src/ca/fraggergames/ffxivextract/gui/components/EXDF_View.java
+++ src/ca/fraggergames/ffxivextract/gui/components/EXDF_View.java
@@ -1,8 +1,12 @@
 package ca.fraggergames.ffxivextract.gui.components;
 
+import java.awt.Component;
+
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.table.AbstractTableModel;
+import javax.swing.table.TableCellRenderer;
+import javax.swing.table.TableColumn;
 
 import ca.fraggergames.ffxivextract.models.EXDF_File;
 import ca.fraggergames.ffxivextract.models.EXDF_StringEntry;
@@ -12,11 +16,36 @@ public class EXDF_View extends JScrollPane{
 	
 	JTable stringTable;
 	
-	public EXDF_View(EXDF_File file) {
+	public EXDF_View(EXDF_File file) {		
 		
 		stringTable = new JTable(new EXDFTableModel(file));
+		stringTable.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
 		getViewport().add(stringTable);
 		
+		for (int column = 0; column < stringTable.getColumnCount(); column++)
+		{
+		    TableColumn tableColumn = stringTable.getColumnModel().getColumn(column);
+		    int preferredWidth = tableColumn.getMinWidth();
+		    int maxWidth = tableColumn.getMaxWidth();
+		 
+		    for (int row = 0; row < stringTable.getRowCount(); row++)
+		    {
+		        TableCellRenderer cellRenderer = stringTable.getCellRenderer(row, column);
+		        Component c = stringTable.prepareRenderer(cellRenderer, row, column);
+		        int width = c.getPreferredSize().width + stringTable.getIntercellSpacing().width;
+		        preferredWidth = Math.max(preferredWidth, width);
+		 
+		        //  We've exceeded the maximum width, no need to check other rows
+		 
+		        if (preferredWidth >= maxWidth)
+		        {
+		            preferredWidth = maxWidth;
+		            break;
+		        }
+		    }
+		 
+		    tableColumn.setPreferredWidth( preferredWidth );
+		}
 	}
 
 	class EXDFTableModel extends AbstractTableModel{
