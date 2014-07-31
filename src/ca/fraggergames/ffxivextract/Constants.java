71bb60e9436946e6df4d44fcaa8b906c261b3e22
Filip Maj <filipmaj@gmail.com>
2014-07-31 10:03:41 -0400

    Added new content types to research. Enabled debug.

--- src/ca/fraggergames/ffxivextract/Constants.java
+++ src/ca/fraggergames/ffxivextract/Constants.java
@@ -5,7 +5,7 @@ public class Constants {
 	public static final String APPNAME = "FFXIV 2.0 Data Explorer";
 	public static final String VERSION = "v0.3";
 	public static final String COMMIT = "0a6d135317176b3e0a6530a06525bd0bab310751";
-	public static boolean DEBUG = false;
+	public static boolean DEBUG = true;
 	
 }
 	
