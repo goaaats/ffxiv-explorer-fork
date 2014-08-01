d336366f7dab72dabd61141d704454400e1c7283
Filip Maj <filipmaj@gmail.com>
2014-08-01 12:35:26 -0400

    Easter egg font wasn't commited, added a valid path for log testing.

--- src/ca/fraggergames/ffxivextract/Main.java
+++ src/ca/fraggergames/ffxivextract/Main.java
@@ -5,6 +5,7 @@ import javax.swing.UIManager;
 import ca.fraggergames.ffxivextract.gui.FileManagerWindow;
 import ca.fraggergames.ffxivextract.helpers.LERandomAccessFile;
 import ca.fraggergames.ffxivextract.helpers.LuaDec;
+import ca.fraggergames.ffxivextract.models.Log_File;
 import ca.fraggergames.ffxivextract.models.SCD_File;
 
 public class Main {
@@ -18,6 +19,12 @@ public class Main {
 		}
 		FileManagerWindow fileMan = new FileManagerWindow(Constants.APPNAME);
 		fileMan.setVisible(true);		
+		
+		try{
+			Log_File log = new Log_File("C:\\Users\\Filip\\Documents\\my games\\FINAL FANTASY XIV - A Realm Reborn\\FFXIV_CHR00400000009B1AC9\\log\\00000003.log");
+			}
+			catch (Exception e){}
 	}
 
+	
 }
