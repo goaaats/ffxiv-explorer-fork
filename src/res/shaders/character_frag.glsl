b1fcc17517ebe9da20489fcdae33610231a7a0a3
Filip Maj <filipmaj@gmail.com>
2015-02-07 11:50:29 -0500

    Fixed character shader being so dark (reverted to older, must have broken something). Still uses new color blender.

--- src/res/shaders/character_frag.glsl
+++ src/res/shaders/character_frag.glsl
@@ -1,4 +1,4 @@
-#version 150
+#version 110
 precision highp float;
 
 varying vec4 vPosition;
@@ -24,17 +24,17 @@ uniform bool uHasSpecular;
 uniform bool uHasColorSet;
 
 vec3 lightPos = vec3(1.0,1.0,1.0);
-vec3 ambientColor = vec3(0.0, 0.0, 0.0);
-vec3 diffuseColor = vec3(0.3, 0.3, 0.3 );
+vec3 ambientColor = vec3(0.1, 0.1, 0.1);
+vec3 diffuseColor = vec3(0.7, 0.7, 0.7);
 vec3 specColor = vec3(1.0, 1.0, 1.0);
 
 void main() {
 	
 	//Color Maps
 	vec4 mapDiffuse = vColor;		
-	vec4 mapMask;
 	vec4 mapNormal;
 	vec4 mapSpecular;
+	vec4 mapMask;
 	vec4 specularColor;	
 	
 	//Color Sets
@@ -78,53 +78,46 @@ void main() {
         
 	vec3 L = normalize(vLightDir);
     vec3 E = normalize(vEyeVec);
+    vec3 R = normalize(2 * dot(L, normal) * normal - L);	
     vec3 H = normalize(L+E);       
     
     //Color
-    vec3 color = table_color.xyz + table_specular.xyz;
-    color = mix(color, table_unknown1.xyz, table_unknown1.a); 
-    
-    //Diffuse         
-    if (uHasDiffuse && uHasNormal && uHasColorSet){            
-    	mapDiffuse = vec4(color * mapDiffuse.xyz,1.0);		
+    vec3 color = table_specular.xyz+table_color.xyz;
+    if (uHasDiffuse && uHasNormal && uHasColorSet){    
+    	mapDiffuse = vec4(color * mapDiffuse.xyz,1.0);    
     } 
-	else if (uHasMask && uHasNormal && uHasColorSet){	
-    	mapDiffuse = vec4(color * mapMask.x, 1.0); //Red Channel Masks Color    	    	
-    }    
-                  
+	else if (uHasMask && uHasNormal && uHasColorSet){
+		specColor = table_specular;	
+    	mapDiffuse = vec4(color * mapMask.x, 1.0);
+    }
+    
+    //Diffuse    
+    mapDiffuse.xyz = mapDiffuse.xyz * max(dot(normal,L),0.0);
     mapDiffuse = clamp(mapDiffuse, 0.0, 1.0);    
-		
-	//Specular
-	specColor = table_specular.xyz;
+	
     float lambertian = max(dot(L,normal), 0.0);
-	float specular = 0.0; 
+	float specular = 0.0;
+ 
 	if(lambertian > 0.0) {
 		// this is blinn phong
 		float specAngle = max(dot(H, normal), 0.0);
+		specular = pow(specAngle, mapDiffuse.z*255);
 		
-		if (uHasDiffuse){
-			specular = pow(specAngle, mapSpecular.z*255.0);
-			specular = mapSpecular.g * mapSpecular.b * specular;
-		}
-		else if (uHasMask)
-			specular = mapSpecular.y;
+		if (!uHasMask)
+			specular = mapSpecular.g * mapSpecular.b * specular;				
 		
 		//Fresnel approximation
 		float F0 = 0.028;
-		float exp = pow(max(0.0, 1.0-dot(H, E)), 5.0);
+		float exp = pow(max(0, 1-dot(H, E)), 5);
 	 	float fresnel = exp+F0*(1.0-exp);
-	//	specular *= fresnel;				
+		//specular *= fresnel;				
 	}
     
     float rimShading = smoothstep(0.8, 1.0, (1.0 - max(dot(E, normal), 0.0)));    
 
-	vec3 calcedSpecular = specular * specColor;
-		
-	mapDiffuse *= 0.9;
-		
-	gl_FragColor = vec4(ambientColor +                      
-                      lambertian * mapDiffuse.xyz +
-                      calcedSpecular, 1.0);
+	gl_FragColor = vec4(ambientColor +
+                      rimShading * mapDiffuse + 
+                      lambertian * mapDiffuse , 1.0);
 }
 
 
